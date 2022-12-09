d21c8c9153e7bd537e3188b4d52c34e735a26794
sinlc <56957789+sinlc@users.noreply.github.com>
2022-12-09 09:35:21 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,11 +4,22 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     m1.move(1)
 })
-let m3: game.LedSprite = null
 let m2: game.LedSprite = null
+let m3: game.LedSprite = null
 let m1: game.LedSprite = null
 m1 = game.createSprite(0, 4)
-game.setScore(10)
+game.setScore(5)
+game.startCountdown(15000)
+basic.forever(function () {
+    m3 = game.createSprite(3, 0)
+    m3.turn(Direction.Right, 90)
+    basic.pause(300)
+    for (let index = 0; index < 4; index++) {
+        m3.move(1)
+        basic.pause(300)
+    }
+    m3.delete()
+})
 basic.forever(function () {
     m2 = game.createSprite(2, 0)
     m2.turn(Direction.Right, 90)
@@ -19,13 +30,11 @@ basic.forever(function () {
     m2.delete()
 })
 basic.forever(function () {
-    m3 = game.createSprite(3, 0)
-    m3.turn(Direction.Right, 90)
-    for (let index = 0; index < 4; index++) {
-        m3.move(1)
-        basic.pause(300)
+    if (m1.isTouching(m3)) {
+        basic.showIcon(IconNames.Sad)
+        game.addScore(-1)
+        m1.set(LedSpriteProperty.X, 0)
     }
-    m3.delete()
 })
 basic.forever(function () {
     if (m1.isTouching(m2)) {
@@ -35,16 +44,9 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (m1.isTouching(m3)) {
-        basic.showIcon(IconNames.Sad)
-        game.addScore(-1)
-        m1.set(LedSpriteProperty.X, 0)
-    }
-})
-basic.forever(function () {
     if (m1.isTouching(game.createSprite(4, 4))) {
         basic.showIcon(IconNames.Happy)
-        game.gameOver()
-        basic.showNumber(game.score())
+        game.addScore(1)
+        m1.set(LedSpriteProperty.X, 0)
     }
 })
